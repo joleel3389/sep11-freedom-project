311838c4b913e8ad2ace9e76b76664f36d38e5fa
Jolee Liu <joleel@nycstudents.net>
2025-11-03 11:41:05 -0500

    move sandbox vue tool work to tool folder

---
+++ tool/vue-js-testing/testing-vue-project/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+    import {ref} from 'vue'
+    const name = ref('Rachel')
+    const emoji = ref('✌🏻')
+    const year = ref('2026')
+</script>
+
+<template>
+    <header>
+        <span>{{emoji}}</span>
+    </header>
+
+    <main>
+        <h1>Hello, {{name}}!</h1>
+        <p>I'm about to learn <a href="https://vuejs.org/" target="_blank">Vue.js</a>!</p>
+    </main>
+
+    <footer>
+        <p> &copy; {{year}} </p>
+    </footer>
+</template>
+
+<style scoped>
+    header span{
+        font-size:3rem;
+    }
+    main h1{
+        display:block;
+    }
+    main a{
+        color: #42B883;
+    }
+    footer {
+        position:fixed;
+        bottom:0;
+        padding-bottom:20px;
+    }
+    footer p{
+        color:blue;
+    }
+</style>
