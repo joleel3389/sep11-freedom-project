b01c326873776d9f094d0bbd3419e3ba05dbd1ac
Jolee Liu <joleel@nycstudents.net>
2025-11-08 14:33:15 -0500

    copy scraped tool work react js

---
+++ tool/react-js-attempt/App.js
@@ -0,0 +1,9 @@
+   function App() {
+        return (
+            <div>
+              <h1>Welcome to my test app</h1>
+              <Home />
+            </div>
+        );
+      }
+
---
+++ tool/react-js-attempt/Home.js
@@ -0,0 +1,7 @@
+   function Home() {
+        return (
+                <button>
+                    Button for my app
+                </button>
+        );
+      }
