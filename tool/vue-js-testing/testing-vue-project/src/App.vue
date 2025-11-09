71147c210bc6097c1200f9e88d8e33c2b3f7e91b
Jolee Liu <joleel@nycstudents.net>
2025-11-08 21:14:05 -0500

    try structural components

--- tool/vue-js-testing/testing-vue-project/src/App.vue
+++ tool/vue-js-testing/testing-vue-project/src/App.vue
@@ -1,35 +1,57 @@
 <script setup>
+    /*
     import {ref} from 'vue'
     const name = ref('Rachel')
     const emoji = ref('✌🏻')
     const year = ref('2026')
+    */
+
+    import Header from '@/components/Header.vue'
+    import Main from '@/components/Main.vue'
+    import Footer from '@/components/Footer.vue'
 </script>
 
 <template>
+    <Header/>
+    <Main/>
+    <Footer/>
+    <!--
     <header>
         <span>{{emoji}}</span>
     </header>
+    -->
 
+    <!--
     <main>
         <h1>Hello, {{name}}!</h1>
         <p>I'm about to learn <a href="https://vuejs.org/" target="_blank">Vue.js</a>!</p>
     </main>
+    -->
 
+    <!--
     <footer>
         <p> &copy; {{year}} </p>
     </footer>
+    -->
 </template>
 
 <style scoped>
+    /*
     header span{
         font-size:3rem;
     }
+    */
+
+    /*
     main h1{
         display:block;
     }
     main a{
         color: #42B883;
     }
+    */
+
+    /*
     footer {
         position:fixed;
         bottom:0;
@@ -38,4 +60,5 @@
     footer p{
         color:blue;
     }
+    */
 </style>
---
+++ tool/vue-js-testing/testing-vue-project/src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import {ref} from 'vue'
+    const emoji = ref('✌🏻')
+</script>
+
+<template>
+    <header>
+        <span>{{emoji}}</span>
+    </header>
+</template>
+
+<style scoped>
+    header span{
+        font-size:3rem;
+    }
+</style>
