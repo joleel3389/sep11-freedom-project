2f90b64689ebc30faef3f46ad70833b2c99b9548
joleel3389 <joleel@nycstudents.net>
2026-04-07 14:45:23 -0400

    navigation change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,7 +12,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0 poppins-regular">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="https://www.desmos.com/calculator" target="_blank">Calculator</a>
+                    <a class="nav-link" aria-current="page" href="/sep11-freedom-project/">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="https://youtu.be/8SoWzkMO1YQ" target="_blank">Secret</a>
