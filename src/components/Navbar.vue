38ff3d01b561948ec4e8e8df6c20f094a3eb4694
joleel3389 <joleel@nycstudents.net>
2026-04-07 14:56:52 -0400

    navbar title change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@
 <template>
     <nav class="navbar fixed-top bg-body-blue navbar-expand-lg">
         <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1 text-dark poppins-regular">Calculus study mini-game</span>
+            <span class="navbar-brand mb-0 h1 text-dark poppins-regular">Calculus study game</span>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
